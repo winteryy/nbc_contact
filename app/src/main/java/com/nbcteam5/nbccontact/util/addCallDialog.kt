8c076151832558de55d72582e00eba5f7e48ba7a
JunHo <bagbamba13@gmail.com>
2024-04-23 19:10:36 +0900

    유효성 검사 작성

--- app/src/main/java/com/nbcteam5/nbccontact/util/addCallDialog.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/util/addCallDialog.kt
@@ -11,16 +11,19 @@ import android.view.Window
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.nbcteam5.nbccontact.databinding.DialogAddBinding
+import com.nbcteam5.nbccontact.util.UserInformation.USER_INFORMATION
 
-@RequiresApi(Build.VERSION_CODES.S)
+
 fun Activity.addCallDialog() {
     val dialogBinding = DialogAddBinding.inflate(layoutInflater)
     val dialog = AlertDialog.Builder(this)
         .setView(dialogBinding.root)
         .create() // Dialog 실제 생성 부분
 
-    val sharedPreferences = getSharedPreferences(UserInformationManager.USER_INFORMATION, Context.MODE_PRIVATE)
+    val sharedPreferences =
+        getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)
     // 모서리 둥글기
     dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
     dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
@@ -32,42 +35,46 @@ fun Activity.addCallDialog() {
         val newEmail = dialogBinding.email.text.toString()
         val newEvent = dialogBinding.event.text.toString()
         // 이미 저장되어 있는 이름과 번호
-        val existingName = sharedPreferences.getString(UserInformationManager.NAME, null)
-        val existingPhoneNumber = sharedPreferences.getString(UserInformationManager.PHONE_NUMBER, null)
+        val existingName = sharedPreferences.getString(UserInformation.NAME, null)
+        val existingPhoneNumber = sharedPreferences.getString(UserInformation.PHONE_NUMBER, null)
 
         if (newName == existingName) {
             Toast.makeText(this, "이미 저장된 이름입니다.", Toast.LENGTH_LONG).show()
         } else if (newPhoneNumber == existingPhoneNumber) {
             Toast.makeText(this, "이미 저장된 번호입니다.", Toast.LENGTH_LONG).show()
+        } else if (!UserInformation.isValidPhoneNumber(newPhoneNumber)) {
+            Toast.makeText(this, "번화 저장 형식이 틀렸습니다.", Toast.LENGTH_LONG).show()
         } else {
             with(sharedPreferences.edit()) {
-                putString(UserInformationManager.NAME, newName)
-                putString(UserInformationManager.PHONE_NUMBER, newPhoneNumber)
-                putString(UserInformationManager.EMAIL, newEmail)
-                putString(UserInformationManager.EVENT, newEvent)
+                putString(UserInformation.NAME, newName)
+                putString(UserInformation.PHONE_NUMBER, newPhoneNumber)
+                putString(UserInformation.EMAIL, newEmail)
+                putString(UserInformation.EVENT, newEvent)
                 apply()
             }
             Toast.makeText(this, "저장이 되었습니다", Toast.LENGTH_LONG).show()
             dialog.dismiss() // 저장 후 대화상자 닫기
+
         }
-    }
 
-    dialogBinding.cancleBtn.setOnClickListener {
-        Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_LONG).show()
-        dialog.dismiss() // 취소 버튼 클릭 후 대화상자 닫기
-    }
 
-    dialog.show()
+        dialogBinding.cancleBtn.setOnClickListener {
+            Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_LONG).show()
+            dialog.dismiss() // 취소 버튼 클릭 후 대화상자 닫기
+        }
 
-    // 다이얼로그의 크기 조정
-    val window = dialog.window
-    if (window != null) {
-        val displayMetrics = resources.displayMetrics.density
-        val dpWidth = 300
-        val dpHeight = 600
-        val width = (displayMetrics * dpWidth).toInt()
-        val height = (displayMetrics * dpHeight).toInt()
+        dialog.show()
 
-        window.setLayout(width, height)
+        // 다이얼로그의 크기 조정
+        val window = dialog.window
+        if (window != null) {
+            val displayMetrics = resources.displayMetrics.density
+            val dpWidth = 300
+            val dpHeight = 600
+            val width = (displayMetrics * dpWidth).toInt()
+            val height = (displayMetrics * dpHeight).toInt()
+
+            window.setLayout(width, height)
+        }
     }
 }
--- app/src/main/java/com/nbcteam5/nbccontact/util/addConst.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/util/addConst.kt
@@ -2,10 +2,15 @@ package com.nbcteam5.nbccontact.util
 
 
 
-object UserInformationManager {
+object UserInformation{
     const val USER_INFORMATION = "userInformation"
     const val NAME = "name"
     const val PHONE_NUMBER = "phoneNumber"
     const val EMAIL = "email"
     const val EVENT = "event"
+
+    fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        val regex = "^01[016789]\\d{3,4}\\d{4}$".toRegex()
+        return phoneNumber.matches(regex)
+    }
 }
