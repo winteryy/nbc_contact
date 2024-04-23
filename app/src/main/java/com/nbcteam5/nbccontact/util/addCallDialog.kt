bd6edf327e4b01bebb161eeb3b73996f470f74ec
JunHo <bagbamba13@gmail.com>
2024-04-23 11:43:27 +0900

    dialog 크기 조정

--- app/src/main/java/com/nbcteam5/nbccontact/util/addCallDialog.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/util/addCallDialog.kt
@@ -2,11 +2,18 @@ package com.nbcteam5.nbccontact.util
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.view.Gravity
+import android.view.Window
 
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.nbcteam5.nbccontact.databinding.DialogAddBinding
 
+@RequiresApi(Build.VERSION_CODES.S)
 fun Activity.addCallDialog() {
     val dialogBinding = DialogAddBinding.inflate(layoutInflater)
     val dialog = AlertDialog.Builder(this)
@@ -14,6 +21,10 @@ fun Activity.addCallDialog() {
         .create() // Dialog 실제 생성 부분
 
     val sharedPreferences = getSharedPreferences(UserInformationManager.USER_INFORMATION, Context.MODE_PRIVATE)
+    // 모서리 둥글기
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+    dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
 
     dialogBinding.saveBtn.setOnClickListener {
         val newName = dialogBinding.name.text.toString()
@@ -47,4 +58,16 @@ fun Activity.addCallDialog() {
     }
 
     dialog.show()
+
+    // 다이얼로그의 크기 조정
+    val window = dialog.window
+    if (window != null) {
+        val displayMetrics = resources.displayMetrics.density
+        val dpWidth = 300
+        val dpHeight = 600
+        val width = (displayMetrics * dpWidth).toInt()
+        val height = (displayMetrics * dpHeight).toInt()
+
+        window.setLayout(width, height)
+    }
 }
