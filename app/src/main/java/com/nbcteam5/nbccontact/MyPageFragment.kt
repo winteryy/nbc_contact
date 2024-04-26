c14d4c4d075d86e07242ebcc8f3116dab8d899c0
jinnnyyy <128459807+jinnnyyy@users.noreply.github.com>
2024-04-26 20:37:51 +0900

    디테일 페이지로 이동

--- app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
@@ -6,15 +6,18 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import coil.load
+import com.nbcteam5.nbccontact.MainActivity
 import com.nbcteam5.nbccontact.R
+import com.nbcteam5.nbccontact.data.ContactData
 import com.nbcteam5.nbccontact.data.ContactDatabase
+import com.nbcteam5.nbccontact.data.UserData
 import com.nbcteam5.nbccontact.databinding.FragmentMyPageBinding
 import kotlin.random.Random
 
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMyPageBinding? = null
     private val binding get() = _binding!!
-
+    private val recommendUserData = getRandomPeople()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,34 +29,32 @@ class MyPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
-        randomPeople()
         binding.contact.setOnClickListener(){
             moveDetail()
         }
     }
+
     private fun moveDetail(){
-        val transaction = parentFragmentManager
-        transaction.replace(R.id.fragment_container, fragment_contact_detail)
-        transaction.commit()
-    }
+        (requireActivity() as MainActivity).detailFromList(recommendUserData)
     }
 
 
     private fun
-            randomPeople(){
+            getRandomPeople():ContactData{
         val userList = ContactDatabase.getContactData()
-        val randomUser = userList[Random.nextInt(userList.size)]
-        binding.ivRvUser.load(randomUser.profileImage)
-        binding.ivRvFavorite.isVisible = randomUser.isFavorite
-        binding.tvRvUserName.text = randomUser.name
+        return userList[Random.nextInt(userList.size)]
 
     }
+
     private fun initViews(){
         val user = ContactDatabase.getUserData()
         binding.name.text = user.name
         binding.number.text = user.phoneNumber
         binding.home.text = user.address
         binding.email.text = user.email
+        binding.ivRvUser.load(recommendUserData.profileImage)
+        binding.ivRvFavorite.isVisible = recommendUserData.isFavorite
+        binding.tvRvUserName.text = recommendUserData.name
     }
 
     override fun onDestroyView() {
