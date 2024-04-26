569a10a0e24f0b5366f0ad66d8e2eafd226f6f0a
jinnnyyy <128459807+jinnnyyy@users.noreply.github.com>
2024-04-26 10:37:37 +0900

    userInfo

--- app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
@@ -2,61 +2,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.google.android.material.imageview.ShapeableImageView
-import com.google.android.material.textview.MaterialTextView
-import com.nbcteam5.nbccontact.R
+import com.nbcteam5.nbccontact.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
-
-    private lateinit var imageView: ShapeableImageView
-    private lateinit var nameTextView: MaterialTextView
-    private lateinit var numberTextView: MaterialTextView
-    private lateinit var homeTextView: MaterialTextView
-    private lateinit var emailTextView: MaterialTextView
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_my_page, container, false)
-        imageView = view.findViewById(R.id.image)
-        nameTextView = view.findViewById(R.id.name)
-        numberTextView = view.findViewById(R.id.number)
-        homeTextView = view.findViewById(R.id.home)
-        emailTextView = view.findViewById(R.id.email)
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val user = getUserInfo()
 
+        binding.name.text = user.name
+        binding.number.text = user.phoneNumber
+        binding.home.text = user.address
+        binding.email.text = user.email
 
-        nameTextView.text = user.name
-        numberTextView.text = user.phoneNumber
-        homeTextView.text = user.address
-        emailTextView.text = user.email
-
-        return view
+//        binding.btn.setOnClickListener { requireActivity().finish() }
     }
 
-//    val btn = findViewById<Button>(R.id.btn)
-//    btn.setOnClickListener{
-//        finish()
-//    }
     private fun getUserInfo(): User {
         return User(
-            name = "새이름",
-            phoneNumber = "010-1234-5678",
-            address = "서울시 여름구 2동",
-            email = "newname@naver.com"
+            name = "아무개",
+            phoneNumber = "010-4321-9876",
+            address = "경기도 스타시 르타동",
+            email = "noonedog@naver.com"
         )
     }
 
-
-    // 사용자 정보 모델 클래스
     data class User(
         val name: String,
         val phoneNumber: String,
         val address: String,
         val email: String
     )
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
