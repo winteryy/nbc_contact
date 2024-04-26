b3ab8f274f062544b4b1d1179ba824e406ed1e9a
jinnnyyy <128459807+jinnnyyy@users.noreply.github.com>
2024-04-26 15:54:44 +0900

    수정사항 고침

--- app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
+++ app/src/main/java/com/nbcteam5/nbccontact/MyPageFragment.kt
@@ -11,7 +11,7 @@ import com.nbcteam5.nbccontact.databinding.FragmentMyPageBinding
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMyPageBinding? = null
     private val binding get() = _binding!!
-    private val contactData : ContactData? = null
+//    private val contactData : ContactData? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,24 +24,34 @@ class MyPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        binding.contact.setOnClickListener(){
+            randompeople()
+        }
+    }
+    private fun randompeople(){
+        val range = (1..11)
+        val randomUser = ContactDatabase.()
+        binding.name.text = randomUser.name
+        binding.number.text = randomUser.phoneNumber
+        binding.home.text = randomUser.address
+        binding.email.text = randomUser.email
     }
     private fun initViews(){
-        val user = getUserInfo()
+        val user = ContactDatabase.getUserData()
         binding.name.text = user.name
         binding.number.text = user.phoneNumber
         binding.home.text = user.address
         binding.email.text = user.email
     }
-    private fun getUserInfo(): UserData {
-        val result = (contactData as ContactDatabase).getUserData()
-        return UserData(
-            name = result.name,
-            phoneNumber = result.phoneNumber,
-            address = result.address,
-            email = result.email
-        )
-    }
-
+//    private fun getUserInfo(): UserData {
+//        val result = (contactData as ContactDatabase).getUserData()
+//        return UserData(
+//            name = result.name,
+//            phoneNumber = result.phoneNumber,
+//            address = result.address,
+//            email = result.email
+//        )
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
